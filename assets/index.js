ec935d7879811c8e093234d42f28ba9eeff6eec1
NatiDa <se.nathan.daniel@gmail.com>
2021-02-15 13:43:00 +0300

    Major changes in html and js file

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+const loginLink = document.querySelector(".loginNavbarLink");
+const logoutLink = document.querySelector(".logoutNavbarLink");
+const dropdown = document.querySelector(".dropdown")
+
+const user_name = document.querySelector(".user_name");
+
+const startFundmeLink = document.querySelector(".startFundMeNavbarLink") 
