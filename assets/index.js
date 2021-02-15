362928ea9acdff8da38f6b7880c56686eb169ecc
NatiDa <se.nathan.daniel@gmail.com>
2021-02-15 15:28:12 +0300

    functions in js and minor html changes

--- assets/index.js
+++ assets/index.js
@@ -5,3 +5,45 @@ const dropdown = document.querySelector(".dropdown")
 const user_name = document.querySelector(".user_name");
 
 const startFundmeLink = document.querySelector(".startFundMeNavbarLink") 
+
+if( location.href!="./Mydonations.html"){
+    //startFundmeLink.style.display = "none";
+    console.log("Mydonations");
+
+}
+
+
+startFundmeLink.addEventListener('click',()=>{
+    if(sessionStorage.getItem("globalEmail")!=null){
+        location.href = "./Mydonations.html"
+    }else{
+        location.href = "./signup.html"
+
+    }
+
+
+})
+
+
+logoutLink.addEventListener('click',()=>{
+    console.log("logout");
+    sessionStorage.clear()
+    location.href = "./index.html"
+
+})
+
+
+
+
+if(sessionStorage.getItem("globalEmail")!=null){
+    console.log("loggedIN");
+    loginLink.style.display = "none";
+    dropdown.style.display = "block";
+    //user_name.innerText = 
+    
+
+}else{
+    console.log("LoggedOut");
+    loginLink.style.display = "block";
+    dropdown.style.display = "none";
+}
