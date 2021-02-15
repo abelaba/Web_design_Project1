249797ef1fbaa9d2dccc35ff7293b378ef8e4188
abelaba <se.abel.abate@gmail.com>
2021-02-15 22:21:19 +0300

    Postdonation added

---
+++ assets/js/mydonation.js
@@ -0,0 +1,10 @@
+const donationsContainer = document.querySelector(".myDonationsContainer");
+
+const saveChangesButton = document.querySelector(".savechanges");
+
+let donationdatabase;
+
+let mytitle, goal, message, mylocation,category;
+
+
+const modalBody = document.querySelector(".modal-body");
