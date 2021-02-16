c2048549e14bfcaef17dbe8f55030a618db8b238
abelaba <se.abel.abate@gmail.com>
2021-02-16 21:18:55 +0300

    onclick open modal added to edit donation

--- assets/js/mydonation.js
+++ assets/js/mydonation.js
@@ -51,6 +51,51 @@ document.addEventListener('DOMContentLoaded',()=>{
                 manage.className = "manage btn btn-primary";
                 manage.setAttribute("data-bs-target","#exampleModal")
                 manage.setAttribute("data-bs-toggle","modal")
+                manage.onclick = function() {
+
+                    
+                    
+                    sessionStorage.setItem("key",element.key);
+                    output = `  <div class="container-fluid">
+                            
+                                    <div class="mb-3">
+                                    <label for="title" class="form-label">Title</label>
+                                    <input type="text" value="${element.data.title}" class="form-control" id="title" aria-describedby="title">
+                                    </div>
+                                    <div class="mb-3">
+                                    <label for="goal" class="form-label">Goal</label>
+                                    <input type="text" value="${element.data.goal}" class="form-control" id="goal">
+                                    </div>
+                                    <div class="mb-3">
+                                    <label for="category" class="form-label">Category</label>
+                                    <input type="text" value="${element.data.category}"class="form-control" id="category">
+                                    </div>
+                                    <div class="mb-3">
+                                    <label for="location" class="form-label">Location</label>
+                                    <input type="text" value="${element.data.location}" class="form-control" id="location">
+                                    </div>
+                                    
+                                    <div class="form-group class="mb-3"">
+                                        <label for="detail" class="form-label"> Your message</label>
+                                        <textarea class="form-control" name="detail" id="message" rows="10" cols='100'>${element.data.message}</textarea>
+                        
+                                    </div>
+                                    
+                                
+                                </div>`
+                    
+                    modalBody.innerHTML = output;
+                    mytitle = modalBody.firstElementChild.children[0].children[1];
+                    goal = modalBody.firstElementChild.children[1].children[1];
+                    category = modalBody.firstElementChild.children[2].children[1];
+                    
+                    mylocation = modalBody.firstElementChild.children[3].children[1];
+                    message = modalBody.firstElementChild.children[4].children[1];
+                      
+                    
+                    
+                    
+                }
                
                 manage.innerHTML = "Manage";
                 
