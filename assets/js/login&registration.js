bc173618d2d58e154ce6a7fce85212cf70ac775a
ruthzaid <se.ruth.zaid@gmail.com>
2021-02-17 20:27:04 +0300

    started js for registration and login (variables only)

---
+++ assets/js/login&registration.js
@@ -0,0 +1,12 @@
+
+//Signup
+const fname = document.querySelector("#firstName")
+const lname = document.querySelector("#lastName")
+const email = document.querySelector("#email")
+const pword = document.querySelector("#password")
+const signupbutton = document.querySelector("#signup-button")
+
+//Login
+const emailL =document.querySelector("#emaillogin-input") 
+const passwordL = document.querySelector("#passwordlogin-input")
+const loginButton = document.querySelector("#login-button")
