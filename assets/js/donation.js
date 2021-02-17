8cc8f078a7baedb66e12143a15b197acbbf92d8c
abelaba <se.abel.abate@gmail.com>
2021-02-17 19:03:41 +0300

    view single donation added to donation.js

---
+++ assets/js/donation.js
@@ -0,0 +1,48 @@
+
+
+let data = JSON.parse(sessionStorage.getItem("data"));
+
+const donationtitle = document.querySelector(".donation-title");
+let message = document.querySelector(".message");
+const locate = document.querySelector(".location");
+const moneycount = document.querySelector(".money-count");
+const description = document.querySelector(".description");
+const progress = document.querySelector(".progress-bar");
+
+
+
+const donateButton = document.querySelector(".donate-button");
+donateButton.addEventListener('click',donateCheck);
+
+let donationdatabase = new Localbase("donationdatabase");
+
+document.addEventListener("DOMContentLoaded",donationview);
+
+
+function donationview() {
+
+    if(sessionStorage.getItem("key")!=null){
+        donationdatabase.collection('donationinfo').doc(sessionStorage.getItem("key")).get().then(document => {
+            donationtitle.innerText = document.title;
+            locate.innerText = document.mylocation;
+            message.innerText = document.message;
+            moneycount.innerHTML = `Br.${document.accumulated} raised <sub>out of Br.${document.goal}</sub>`;
+            progress.style.width = `${parseInt(document.accumulated)/parseInt(document.goal)*100}%`;
+
+            console.log(parseInt(document.accumulated)/parseInt(document.goal)*100)
+          })
+    }
+    
+}
+
+
+
+
+function donateCheck(){
+    if(sessionStorage.getItem("globalEmail")!=null){
+        location.href = "./checkout.html"
+    }
+    else{
+        location.href = "./signup.html";
+    }
+}
