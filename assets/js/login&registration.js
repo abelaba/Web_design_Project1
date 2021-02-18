3826ddfc5fe8797da7f09b08e8e7deeef9cfc2b8
ruthzaid <se.ruth.zaid@gmail.com>
2021-02-18 14:40:39 +0300

    local base created

--- assets/js/login&registration.js
+++ assets/js/login&registration.js
@@ -1,5 +1,5 @@
 
-//Signup
+//Registration
 const fname = document.querySelector("#firstName")
 const lname = document.querySelector("#lastName")
 const email = document.querySelector("#email")
@@ -10,3 +10,23 @@ const signupbutton = document.querySelector("#signup-button")
 const emailL =document.querySelector("#emaillogin-input") 
 const passwordL = document.querySelector("#passwordlogin-input")
 const loginButton = document.querySelector("#login-button")
+
+let userInfoDatabase;
+let emails = [];
+
+document.addEventListener('DOMContentLoaded',()=>{
+    userInfoDatabase = new Localbase('userInfoDatabase');
+    
+    userInfoDatabase.collection('users').get().then(users => {
+        console.log(users)
+
+        users.forEach(element => {
+            emails.push(element.email);
+        });
+        
+      })
+    
+});
+
+if(email!=null){signupbutton.addEventListener('click',signup);}
+else if (emailL!=null) {loginButton.addEventListener('click',login);} 
