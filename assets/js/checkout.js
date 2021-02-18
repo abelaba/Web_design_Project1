f8e83550553d2e6d4c2b4bbc396cc408d1decaf3
NatiDa <se.nathan.daniel@gmail.com>
2021-02-18 16:31:06 +0300

    few variables declared

---
+++ assets/js/checkout.js
@@ -0,0 +1,12 @@
+const donation = document.querySelector(".donationinput");
+const inputname = document.querySelector(".name");
+const card = document.querySelector(".card");
+const expiry = document.querySelector(".expiry");
+const cvv = document.querySelector(".cvv");
+
+const acceptButton = document.querySelector(".button");
+
+
+
+let transaction;
+let donations;
